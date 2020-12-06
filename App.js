7f782d76c0c317a623614400df58eb0ccba2376e
parksohyeon <dso753@elinmedia.com>
2020-12-07 00:47:57 +0900

    Loading Screen

--- App.js
+++ App.js
@@ -7,44 +7,10 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Loading from './Loading'
 
 const App: () => React$Node = () => {
-  return (
-      <View style={styles.container}>
-        <View style={styles.yellowView} />
-        <View style={styles.blueView} />
-      </View>
-  );
+  return <Loading/>
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  yellowView: {
-    backgroundColor: 'yellow',
-    flex: 1
-  },
-  blueView: {
-    backgroundColor: 'blue',
-    flex: 3
-  }
-});
 
 export default App;
