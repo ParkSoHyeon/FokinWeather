4ea9127d3d14dae9871968dff964e9a5ac7297bd
parksohyeon <dso753@elinmedia.com>
2020-12-08 23:43:16 +0900

    Getting the Weather

--- App.js
+++ App.js
@@ -7,41 +7,49 @@
  */
 
 import React from 'react';
-import { Alert } from 'react-native';
+import {Alert} from 'react-native';
 import Loading from './Loading';
+import axios from 'axios';
 import Geolocation from 'react-native-geolocation-service';
 
-export default class extends React.Component {
-  state = {
-    isLoading: true
-  }
-  getLocation = async () => {
-    try {
-      await Geolocation.requestAuthorization('always');
-      await Geolocation.getCurrentPosition(
-          (position) => {
-            const { coords: { longitude, latitude } } = position;
-            console.log(`longitude: ${longitude}`);
-            console.log(`latitude: ${latitude}`);
-            this.setState({isLoading: false});
-          },
-          (error) => {
-            console.error(error);
-          },
-          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-      );
-    }
-    catch (e) {
-      console.log(e);
-      Alert.alert('Can\'t find you', 'So sad')
-    }
-  }
-  componentDidMount(): * {
-    this.getLocation();
-  }
+const API_KEY = 'ea3fda10afa8f0e3f7e502a57dd83a75';
 
-  render(): React$Node {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading/> : null;
-  }
+export default class extends React.Component {
+    state = {
+        isLoading: true,
+    };
+    getWeather = async (latitude, longitude) => {
+        const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+        console.log(`data: ${JSON.stringify(data)}`);
+    };
+    getLocation = async () => {
+        try {
+            await Geolocation.requestAuthorization('always');
+            await Geolocation.getCurrentPosition(
+                (position) => {
+                    const {coords: {longitude, latitude}} = position;
+                    console.log(`longitude: ${longitude}`);
+                    console.log(`latitude: ${latitude}`);
+                    this.getWeather(latitude, longitude);
+                    this.setState({isLoading: false});
+                },
+                (error) => {
+                    console.error(error);
+                },
+                {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+            );
+        } catch (e) {
+            console.log(e);
+            Alert.alert('Can\'t find you', 'So sad');
+        }
+    };
+
+    componentDidMount(): * {
+        this.getLocation();
+    }
+
+    render(): React$Node {
+        const {isLoading} = this.state;
+        return isLoading ? <Loading/> : null;
+    }
 }
