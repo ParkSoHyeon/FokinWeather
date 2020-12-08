7353525b3b22e5af0181d29b8535ebcc070474bb
parksohyeon <dso753@elinmedia.com>
2020-12-08 23:20:54 +0900

    Getting the Location and Asking for Permissions

--- App.js
+++ App.js
@@ -7,10 +7,41 @@
  */
 
 import React from 'react';
-import Loading from './Loading'
+import { Alert } from 'react-native';
+import Loading from './Loading';
+import Geolocation from 'react-native-geolocation-service';
 
-const App: () => React$Node = () => {
-  return <Loading/>
-};
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+  getLocation = async () => {
+    try {
+      await Geolocation.requestAuthorization('always');
+      await Geolocation.getCurrentPosition(
+          (position) => {
+            const { coords: { longitude, latitude } } = position;
+            console.log(`longitude: ${longitude}`);
+            console.log(`latitude: ${latitude}`);
+            this.setState({isLoading: false});
+          },
+          (error) => {
+            console.error(error);
+          },
+          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+      );
+    }
+    catch (e) {
+      console.log(e);
+      Alert.alert('Can\'t find you', 'So sad')
+    }
+  }
+  componentDidMount(): * {
+    this.getLocation();
+  }
 
-export default App;
+  render(): React$Node {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading/> : null;
+  }
+}
---
+++ geolocation.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  FokinWeather
+//
+//  Created by 박소현 on 2020/12/08.
+//
+
+import Foundation
